a974f3bdaee3239d6101050f254c36028f387b39
Seweryn Golba <sgoolb@gmail.com>
2023-08-18 18:40:50 +0200

    projects data

---
+++ src/data/projectsData.js
@@ -0,0 +1,12 @@
+const projectsData = [
+  {
+    image: "../../assets/projectone.png",
+    name: "HOME BUDGET",
+    description:
+      "The Home Budget App is a simple web application that helps you track your earnings and expenses, providing a clear overview of your financial situation.",
+    github: "https://github.com/seweryngolba/home-budget",
+    demo: "https://homebudgetapp-seweryn-golba.netlify.app/",
+  },
+];
+
+export default projectsData;
