b97d2da3967acb6b4a5c679bf0c525184a592c58
Seweryn Golba <sgoolb@gmail.com>
2023-08-18 18:26:58 +0200

    projects base

---
+++ src/components/MyProjects/MyProjects.js
@@ -0,0 +1,20 @@
+import "./MyProjects.css";
+
+const MyProjects = () => {
+  return (
+    <div className="projectsContainer">
+      <h2>MY PROJECTS</h2>
+      <div className="projectBox">
+        <img></img>
+        <h3>HOME BUDGET</h3>
+        <p>lorem ipsum lorem ipsum</p>
+        <div className="projectButtons">
+          <a>GITHUB</a>
+          <a>DEMO</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyProjects;
