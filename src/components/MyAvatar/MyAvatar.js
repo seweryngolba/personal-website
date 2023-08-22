2255ae8d93670ee8fe72f0ab145368cbe0ea313b
Seweryn Golba <sgoolb@gmail.com>
2023-08-22 21:21:28 +0200

    avatar done

--- src/components/MyAvatar/MyAvatar.js
+++ src/components/MyAvatar/MyAvatar.js
@@ -2,20 +2,22 @@ import React from "react";
 import { Avatar } from "@readyplayerme/visage";
 
 const modelSrc = "https://models.readyplayer.me/64dcddc1cfdd0f000df9468c.glb";
-const animationSrc = "/waving-animation.glb";
+const animationSrc = "/Waving.fbx";
 
 const MyAvatar = () => {
   return (
-    <Avatar
-      animationSrc={animationSrc}
-      cameraInitialDistance={2.5}
-      cameraTarget={1.55}
-      modelSrc={modelSrc}
-      onLoadedAnimation={{
-        loop: 1,
-        src: "/Waving.fbx",
-      }}
-    />
+    <div className="myAvatar">
+      <Avatar
+        animationSrc={animationSrc}
+        cameraInitialDistance={2.2}
+        cameraTarget={0.5}
+        modelSrc={modelSrc}
+        onLoadedAnimation={{
+          loop: 1,
+          src: "/Waving.fbx",
+        }}
+      />
+    </div>
   );
 };
 
