0e20798674de66e2b8df9d3fccfcc97fe09c2961
Seweryn Golba <sgoolb@gmail.com>
2023-08-30 23:22:02 +0200

    new contact section

--- src/components/SpeedGame/SpeedGame.js
+++ src/components/SpeedGame/SpeedGame.js
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from "react";
 import "./SpeedGame.css"; // Import your CSS file
 
 const SpeedGame = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="containerContact">
+      <div className="gameBox">
+        <h2>CONTACT</h2>
+        <div className="realInfo">
+          <p>SEWERYN GOLBA</p>
+          <p>EMAIL: SGOOLB@GMAIL.COM</p>
+          <p>TEL: 881 515 126</p>
+        </div>
+      </div>
+      <div className="myPhoto"></div>
+    </div>
+  );
 };
 
 export default SpeedGame;
